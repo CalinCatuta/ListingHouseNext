f1039dfbc6c22d0287ef1d0e47b89e002177f166
CalinCatuta <calinut123@yahoo.co.uk>
2024-03-08 17:00:02 +0200

    PropertCard done.

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,4 +1,22 @@
+import properties from "@/properties.json";
+// components
+import PropertyCard from "@/components/PropertyCard";
+
 const PropertiesPage = () => {
-  return <div>PropertiesPage</div>;
+  return (
+    <section class="px-4 py-6">
+      <div class="container-xl lg:container m-auto px-4 py-6">
+        {properties.length === 0 ? (
+          <p>No properties found</p>
+        ) : (
+          <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
 };
 export default PropertiesPage;
