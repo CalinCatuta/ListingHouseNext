fcb868cef43e469def006e209312add56701ddc9
CalinCatuta <calinut123@yahoo.co.uk>
2024-03-08 17:20:20 +0200

    Add spinner and HomeProperites.

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,13 @@
 // components
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
-
+import HomeProperties from "@/components/HomeProperties";
 const HomePage = () => {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
     </>
   );
 };
--- app/properties/[id]/page.jsx
+++
@@ -1,4 +0,0 @@
-const CardPage = () => {
-  return <div>CardPage</div>;
-};
-export default CardPage;
