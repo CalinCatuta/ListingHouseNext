167419f7541f5e24e88d4e68f2b591b295482e8a
CalinCatuta <calinut123@yahoo.co.uk>
2024-03-12 18:33:07 +0200

    Hard coded setup

---
+++ app/api/properties/[id]/route.js
@@ -0,0 +1,22 @@
+// MONGO
+import connectDB from "@/config/database";
+// Modal
+import Property from "@/models/Property";
+
+// GET /api/properties/:id
+export const GET = async (request, { params }) => {
+  try {
+    await connectDB();
+
+    const property = await Property.findById(params.id);
+
+    if (!property) return new Response("Property Not Found", { status: 404 });
+
+    return new Response(JSON.stringify(property), {
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response("Something went wrong", { status: 500 });
+  }
+};
