3886144382018f0a0756a412c1347c8493822565
CalinCatuta <calinut123@yahoo.co.uk>
2024-03-12 15:01:39 +0200

    Create our Modals.

---
+++ app/api/properties/route.js
@@ -0,0 +1,15 @@
+// MONGO
+import connectDB from "@/config/database";
+
+export const GET = async (request) => {
+  try {
+    await connectDB();
+
+    return new Response(JSON.stringify({ message: "Hello Word" }), {
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response("Something went wrong", { status: 500 });
+  }
+};
