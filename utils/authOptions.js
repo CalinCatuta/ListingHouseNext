e120c2614b8bb2abdb3f1da620460e11aa33b5af
CalinCatuta <calinut123@yahoo.co.uk>
2024-03-13 17:38:21 +0200

    finish Login and LogOut

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,3 +1,6 @@
+import connectDB from "@/config/database";
+import User from "@/models/User";
+
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
@@ -19,15 +22,31 @@ export const authOptions = {
     // invoce on successful signin
     async signIn({ profile }) {
       // Connect to database
+      await connectDB();
       // Check if user exists
+      const userExists = await User.findOne({ email: profile.email });
       // if not, then add user to db
+      if (!userExists) {
+        // truncate user name if too long
+        const username = profile.name.slice(0, 20);
+
+        await User.create({
+          email: profile.email,
+          username,
+          image: profile.picture,
+        });
+      }
       // Return true to allow sing in
+      return true;
     },
     // modifies the session object
     async session({ session }) {
       // get user
+      const user = await User.findOne({ email: session.user.email });
       // assign the user id to the session
+      session.user.id = user._id.toString();
       // return session
+      return session;
     },
   },
 };
